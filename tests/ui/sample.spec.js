e139de28e241a7b793ae29ecacca2a9d27e82df1
Supriya2k <46884117+Supriya2k@users.noreply.github.com>
2026-04-27 10:47:11 +0200

    Add login test using Playwright framework

--- tests/ui/sample.spec.js
+++ tests/ui/sample.spec.js
@@ -1,1 +1,12 @@
+const { test, expect } = require('@playwright/test');
+const LoginPage = require('../../pages/LoginPage');
 
+test('Login test', async ({ page }) => {
+  const login = new LoginPage(page);
+
+  await page.goto('https://automationexercise.com/login');
+
+  await login.login('test@test.com', 'password');
+
+  await expect(page).toHaveURL(/login/);
+});
